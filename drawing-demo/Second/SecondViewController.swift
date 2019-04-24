16126bc2e1bced2ba94dcaa87aef7e715d41f22e
Ben Norris <code@bsn.design>
2019-04-24 10:06:50 -0600

    Set up blank project

---
+++ drawing-demo/Second/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  drawing-demo
+//
+//  Created by Ben Norris on 4/24/19.
+//  Copyright © 2019 Ben Norris. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ drawing-demo/Shared/MainTabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  MainTabBarController.swift
+//  drawing-demo
+//
+//  Created by Ben Norris on 4/24/19.
+//  Copyright © 2019 Ben Norris. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+}
