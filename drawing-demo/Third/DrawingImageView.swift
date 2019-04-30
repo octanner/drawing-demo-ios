9eb66e28a0b11d6e1e070378854c748c47ba739f
colejoplin <cole_joplin@yahoo.com>
2019-04-30 12:08:38 -0600

    Recording audio and images finished.

--- drawing-demo/Third/DrawingImageView.swift
+++ drawing-demo/Third/DrawingImageView.swift
@@ -3,51 +3,38 @@
 //  drawing-demo
 //
 //  Created by Cole Joplin on 4/28/19.
-//  Copyright © 2019 Ben Norris. All rights reserved.
+//  Copyright © 2019 O. C. Tanner. All rights reserved.
 //
 
 import Foundation
 import UIKit
 
-@IBDesignable
+protocol DrawingImageViewDelegate {
+    func didHaveDrawingError()
+}
+
+/// Draws an image
 class DrawingImageView: UIImageView {
     
-    @IBInspectable
-    var borderWidth: CGFloat {
-        get {
-            return layer.borderWidth
-        }
-        set {
-            layer.borderWidth = newValue
-        }
-    }
+    // MARK: - Properties
     
-    @IBInspectable
-    var borderColor: UIColor? {
-        get {
-            if let color = layer.borderColor {
-                return UIColor(cgColor: color)
-            }
-            return nil
-        }
-        set {
-            if let color = newValue {
-                layer.borderColor = color.cgColor
-            } else {
-                layer.borderColor = nil
-            }
-        }
-    }
+    var delegate: DrawingImageViewDelegate?
     
+    // Drawing
     let pi = CGFloat(Double.pi)
     let forceSensitivity: CGFloat = 4.0
-    
-//    var pencilTexture = UIColor.black
     var pencilTexture = UIColor(patternImage: UIImage(named: "PencilTexture")!)
-
-    let defaultLineWidth : CGFloat = 60
+    let defaultLineWidth : CGFloat = 10
     
+    // Image properties
     var images = [UIImage]()
+    
+    override func awakeFromNib() {
+        layer.borderColor = UIColor.black.cgColor
+        layer.borderWidth = 2.0
+    }
+    
+    // MARK: - Events
     
     override public func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else {
@@ -70,55 +57,47 @@ class DrawingImageView: UIImageView {
             drawStroke(context: context, touch: touch)
         }
         
+        //TODO: Add predictive for better drawing performance
+        
         image = UIGraphicsGetImageFromCurrentImageContext()
         
-        if image != nil {
-            images.append(image!)
-            // Create path.
-            let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-            if let filePath = paths.first?.appendingPathComponent("MyImageName.png") {
-                do {
-                    try image!.pngData()?.write(to: filePath, options: .atomic)
-                } catch {
-                    // Handle the error
-                }
-            }
+        if let drawnImage = image {
+            images.append(drawnImage)
+            writeImageFile(imageToWrite: drawnImage, number: images.count)
         }
         UIGraphicsEndImageContext()
     }
     
+    // MARK: - Drawing Methods
+    
     func drawStroke(context: CGContext?, touch: UITouch) {
         let previousLocation = touch.previousLocation(in: self)
         let location = touch.location(in: self)
-        
-        var lineWidth : CGFloat = 1.0
-        
-        if touch.type == .stylus {
-            lineWidth = lineWidthForDrawing(context: context, touch: touch)
-//            UIColor.darkGray.setStroke()
-            pencilTexture.setStroke()
+
+        if let context = context {
+            let lineWidth = lineWidthForDrawing(context: context, touch: touch)
+            if touch.type == .stylus {
+                pencilTexture.setStroke()
+            }
+            context.setLineWidth(lineWidth)
+            context.setLineCap(.round)
+            context.move(to: previousLocation)
+            context.addLine(to: location)
+            context.strokePath()
         }
-        
-        context!.setLineWidth(lineWidth)
-        context!.setLineCap(.round)
-        
-        context?.move(to: previousLocation)
-        context?.addLine(to: location)
-        
-        context!.strokePath()
     }
     
     func lineWidthForDrawing(context: CGContext?, touch: UITouch) -> CGFloat {
-        var lineWidth = defaultLineWidth
-        
-        if touch.force > 0 {
-            lineWidth = touch.force * forceSensitivity
-        }
-        
-        return lineWidth
+        //TODO: Add azimuth
+//        var lineWidth = defaultLineWidth
+//        if touch.force > 0 {
+//            lineWidth = touch.force * forceSensitivity
+//        }
+//        return lineWidth
+        return defaultLineWidth
     }
     
-    func clearCanvas(_ animated: Bool) {
+    func clearCanvas(animated: Bool) {
         if animated {
             UIView.animate(withDuration: 0.5, animations: {
             }, completion: { finished in
@@ -129,4 +108,20 @@ class DrawingImageView: UIImageView {
         }
         images.removeAll()
     }
+    
+    // MARK: - File Methods
+    
+    func writeImageFile(imageToWrite: UIImage, number: Int) {
+        DispatchQueue.global(qos: .background).async {
+            let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+            if let filePath = paths.first?.appendingPathComponent("drawing\(number).png") {
+                do {
+                    try imageToWrite.pngData()?.write(to: filePath, options: .atomic)
+                } catch {
+                    self.delegate?.didHaveDrawingError()
+                }
+            }
+        }
+    }
+    
 }
--- drawing-demo/Third/ThirdViewController.swift
+++ drawing-demo/Third/ThirdViewController.swift
@@ -9,22 +9,153 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ThirdViewController: UIViewController {
     
-    @IBOutlet weak var drawingCanvas: DrawingImageView!
+    // MARK: - Properties
     
+    @IBOutlet weak var drawingCanvas: DrawingImageView!
     @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    
+    // AVFoundation
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+    var isRecortding = false
+    
+    // MARK: Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func clearTapped(_ sender: Any) {
-        drawingCanvas.clearCanvas(true)
+        clearFiles()
+        configureAudio()
     }
     
+    // MARK: - IBActions
+
+    @IBAction func clearTapped(_ sender: Any) {
+        drawingCanvas.clearCanvas(animated: true)
+        clearFiles()
+    }
+    
+    @IBAction func recordTapped(_ sender: Any) {
+        if audioRecorder == nil {
+            startAudioRecording()
+        } else {
+            finishAudioRecording(success: true)
+        }
+    }
+    
+}
+
+extension ThirdViewController {
+    
+    func clearFiles() {
+        DispatchQueue.global(qos: .background).async {
+            let fileManager = FileManager.default
+            let myDocuments = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+            do {
+                try fileManager.removeItem(at: myDocuments)
+            } catch {
+                return
+            }
+        }
+    }
+    
+    // MARK: - Audio Methods
+    
+    func audioFileURL() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("audio.m4a")
+        return path as URL
+    }
+    
+    func configureAudio() {
+        recordingSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission { (success) in
+                if success {
+                    self.recordButton.isEnabled = true
+                } else {
+                    self.recordButton.isEnabled = false
+                    self.showAudioSessionError(message: "We do not have permission to access the microphone. PLease enable it in Settings.")
+                }
+            }
+        } catch {
+            self.recordButton.isEnabled = false
+            self.showAudioSessionError(message: "Something went wrong setting up the microphone.")
+        }
+    }
+    
+    func showAudioSessionError(message: String) {
+        self.recordButton.isEnabled = false
+        let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Okay", style: .default, handler: nil)
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func startAudioRecording() {
+        let audioFilename = audioFileURL()
+        
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+            recordButton.setTitle("Stop", for: .normal)
+        } catch {
+            finishAudioRecording(success: false)
+        }
+    }
+    
+    func finishAudioRecording(success: Bool) {
+        audioRecorder.stop()
+        audioRecorder = nil
+        recordButton.setTitle("Record", for: .normal)
+        
+        // If things go bad, try to start over.
+        if !success {
+            let alertController = UIAlertController(title: "Error", message: "Something went wrong with the recording. Please try again.", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Okay", style: .default) { (action:UIAlertAction) in
+                self.drawingCanvas.clearCanvas(animated: true)
+                self.clearFiles()
+            }
+            alertController.addAction(action)
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+}
+
+extension ThirdViewController: AVAudioRecorderDelegate {
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishAudioRecording(success: false)
+        }
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
+        print("Error while recording audio \(error!.localizedDescription)")
+    }
+}
+
+extension ThirdViewController: DrawingImageViewDelegate {
+    
+    func didHaveDrawingError() {
+        DispatchQueue.main.async {
+            self.finishAudioRecording(success: false)
+        }
+    }
 }
 
 
